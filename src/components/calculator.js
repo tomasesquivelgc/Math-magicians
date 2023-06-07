70f6611bfd44dc5770c79a587ee2cf75b09bb8e9
TomasEsquivel <117373513+tomasesquivelgc@users.noreply.github.com>
2023-06-07 13:47:05 -0300

    Calculator css add

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Calculator.css';
 
 // Button component
-function Button({ value }) {
-  return <button type="button">{value}</button>;
+function Button({ value, className }) {
+  return (
+    <button type="button" className={className}>
+      {value}
+    </button>
+  );
 }
 
 Button.propTypes = {
   value: PropTypes.string.isRequired,
+  className: PropTypes.string,
+};
+
+Button.defaultProps = {
+  className: 'button',
 };
 
 // Calculator component
 function Calculator() {
   return (
-    <div>
-      <div>0</div>
-      <div>
+    <div className="calculator">
+      <div className="calculator-display">0</div>
+      <div className="buttons">
         <Button value="AC" />
         <Button value="+/-" />
         <Button value="%" />
@@ -32,7 +42,7 @@ function Calculator() {
         <Button value="2" />
         <Button value="3" />
         <Button value="+" />
-        <Button value="0" />
+        <Button value="0" className="double-button" />
         <Button value="." />
         <Button value="=" />
       </div>
